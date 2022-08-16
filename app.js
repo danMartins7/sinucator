d487ee282230c8adc4d074eb4083904d5edd6e35
unknown <danilo.blmartins@gmail.com>
2022-08-15 21:22:35 -0300

    alteração de arrays e introdução ao find

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 const { DB } = require("./db");
 const { Player } = require("./entities/player");
-const { Match } = require("./entities/match")
-const { Season } = require("./entities/season")
-const { SeasonType } = require("./entities/seasonType")
-
-const sType1 = new SeasonType("par/impar", "individual")
-const sType2 = new SeasonType("par/impar", "dupla")
-
-DB.saveSeasonType(sType1);
-DB.saveSeasonType(sType2);
-
-const s1 = new Season(1,"S1","28/06","30/06",[])
-DB.saveSeason(s1)
-
-const p1 = new  Player("P1", "P1",[])
-const p2 = new Player("P2", "P2", [])
+const { Match } = require("./entities/match");
+const { Season } = require("./entities/season");
+const { SeasonType } = require("./entities/seasonType");
+const { PlayerMatch } = require("./entities/playerMatch");
 
 
-const m1 = new Match()
+DB.savePlayer(new Player("P1", "P1"));
+DB.savePlayer(new Player("P2", "P2"));
 
-DB.players.push(p1)
-DB.players.push(p2)
-console.log(s1);
+const seasonType = DB.seasonTypes.find(x => x.type == "3 bolinhas" && x.mode == "individual")
+DB.saveSeason(new Season(seasonType.id,"S1","28/06","30/06"))
+
+const S1 = DB.seasons.find(x => x.name == "S1")
+const M1 = new Match(S1.id)
+DB.saveMatch(M1)
+
+const p1 = DB.players.find(x => x.name == "P1")
+const p2 = DB.players.find(x => x.name == "P2")
+
+var pm1 = new PlayerMatch(p1.id, M1.id, true);
+var pm2 = new PlayerMatch(p2.id, M1.id, false);
+
+DB.savePlayerMatch(pm1);
+DB.savePlayerMatch(pm2);
+
+console.log(DB)
+
+//temporada $temp, modalidade: $modalidade
+// partida entre jogadores: $jogador nome, $jogadorNome
+//vencedor: $jogadorNome
+
+console.log("temporada" + S1.name + ", modalidade : " + seasonType + " " + seasonType.mode);
+
+//const m1 = new Match(1,)
+
+//DB.players.push(p1)
+//DB.players.push(p2)
+//console.log(s1);
--- db.js
+++ db.js
@@ -1,4 +1,8 @@
-function DB(){
+const { Season } = require("./entities/season");
+const { SeasonType } = require("./entities/seasonType");
+const { match } = require("./entities/match")
+
+function DB() {
     this.matches = [];
     this.players = [];
     this.playerMatches = [];
@@ -7,36 +11,44 @@ function DB(){
     this.seasonRakingPlayers = [];
     this.seasonTypes = [];
 
-    this.saveMatch = function(match){
+    // cadastro de valores fixos da aplicaçao
+    this.init = function () {
+        this.saveSeasonType(new SeasonType("par/impar", "individual"));
+        this.saveSeasonType(new SeasonType("par/impar", "dupla"));
+        this.saveSeasonType(new SeasonType("3 bolinhas", "individual"));
+    }
+
+    this.saveMatch = function (match) {
         match.id = this.matches.length + 1;
         this.matches.push(match);
     }
-    this.savePlayer = function(player){
+    this.savePlayer = function (player) {
         player.id = this.players.length + 1;
         this.players.push(player);
     }
-    this.savePlayerMatch = function(playerMatch){
+    this.savePlayerMatch = function (playerMatch) {
         playerMatch.id = this.playerMatches.length + 1;
         this.playerMatches.push(playerMatch);
     }
-    this.saveSeason = function(season){
+    this.saveSeason = function (season) {
         season.id = this.seasons.length + 1;
         this.seasons.push(season);
     }
-    this.saveSeasonRaking = function(seasonRaking){
+    this.saveSeasonRaking = function (seasonRaking) {
         seasonRaking.id = this.seasonRakings.length + 1;
         this.seasonRakings.push(seasonRaking);
     }
-    this.saveSeasonRakingPlayer = function(seasonRakingPlayer){
+    this.saveSeasonRakingPlayer = function (seasonRakingPlayer) {
         this.seasonRakingPlayer.id = this.seasonRakingPlayers.length + 1;
         this.seasonRakingPlayers.push(seasonRakingPlayer);
     }
-    this.saveSeasonType = function(seasonType){
+    this.saveSeasonType = function (seasonType) {
         seasonType.id = this.seasonTypes.length + 1;
         this.seasonTypes.push(seasonType);
     }
 
     return this;
 }
-
-exports.DB = new DB ();
+const db = new DB();
+db.init();
+exports.DB = db;
